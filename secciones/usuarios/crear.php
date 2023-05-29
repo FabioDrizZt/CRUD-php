04109d470d9dfcf850d4822404d0b45ee22b2963
Fabio <>
2023-05-29 20:44:07 -0300

    Creada la funcionalidad de crear usuario

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,4 +1,22 @@
-<?php require_once("../../templates/header.php") ?>
+<?php
+if ($_POST) {
+    print_r($_POST);
+    require_once("../../bd.php");
+    // Recolectar los datos del metodo POST
+    $usuario = (isset($_POST["usuario"]) ? $_POST["usuario"] : "");
+    $password = (isset($_POST["password"]) ? $_POST["password"] : "");
+    $correo = (isset($_POST["correo"]) ? $_POST["correo"] : "");
+    // Preparar la inserción de los datos
+    $sentencia = $conexion->prepare("INSERT INTO `tbl_usuarios` (`id`, `usuario`, `password`, `correo`) 
+                                                VALUES (NULL, :usuario, :password, :correo);");
+    // Asignamos los valores que vienen del metodo POST a la consulta
+    $sentencia->bindValue(":usuario", $usuario);
+    $sentencia->bindValue(":password", $password);
+    $sentencia->bindValue(":correo", $correo);
+    $sentencia->execute();
+    header("Location:index.php");
+}
+require_once("../../templates/header.php") ?>
 <div class="card">
     <div class="card-header">
         Usuarios
@@ -6,9 +24,9 @@
     <div class="card-body">
         <form action="" method="post">
             <div class="mb-3">
-                <label for="nombredelpuesto" class="form-label">Nombre del usuario</label>
-                <input type="text" class="form-control" name="nombredelpuesto" id="nombredelpuesto"
-                    aria-describedby="helpId" placeholder="">
+                <label for="usuario" class="form-label">Nombre del usuario</label>
+                <input type="text" class="form-control" name="usuario" id="usuario" aria-describedby="helpId"
+                    placeholder="">
                 <div class="mb-3">
                     <label for="password" class="form-label">Contraseña</label>
                     <input type="password" class="form-control" name="password" id="password" placeholder="">
